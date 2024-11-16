4f4331ad0cb42bbf9b5ff4917afe244b28a549d2
Sara del Pino Cabrera Sánchez <90867506+SaraCS21@users.noreply.github.com>
2024-11-16 14:55:02 +0000

    Modificar .env.example y readme

--- libriTrackBE/config/db.config.js
+++ libriTrackBE/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
-    USER: "root",
-    PASSWORD: "1234",
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
     DB: "libritrack_db",
     dialect: "mysql",
     pool: {
